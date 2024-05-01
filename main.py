06df68818a6dead06e55b46be12790432c30e562
Malika <malikaaliqulova665@gmail.com>
2024-05-01 16:32:55 +0500

    task

---
+++ main.py
@@ -0,0 +1,27 @@
+# lst1 = ([1, 2, 3, 4, 5], 3)
+# check = int(input('number: '))
+# def lst(lst1):
+#     if check in lst1
+#         return True
+#     else:
+#         return False
+
+#2-mashq-----------
+# def list_less(lst2):
+#     for x in lst2:
+#         resault = x + x <= 100
+#         return resault
+# print(list_less([5, 57]))
+# print(list_less([77, 30]))
+# print(list_less([0]))
+
+#3-mashq--------
+# def max_total(lst3):
+#     if max_total in
+
+
+
+
+
+
+
